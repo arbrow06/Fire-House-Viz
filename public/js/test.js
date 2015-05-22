5f19a282bd3a949d4d9cac12eb6c812eeace63d7
arbrow06 <arbrow06@syr.edu>
2015-05-22 03:06:48 -0400

    Got map to drop placemarkers, working on centering view about persons current location and changing data shown based on map buttons to change datasets shown.

--- public/js/test.js
+++ public/js/test.js
@@ -1,65 +1,133 @@
-var margin = {top:40,right:20,bottom:30,left:40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var formatPercent = d3.format(".0%");
-
-var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .1);
-var y = d3.scale.linear()
-        .range([height, 0]);
-var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
-var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left")
-            .tickFormat(formatPerfect);
-var tip = d3.tip()
-          .attr('class','d3-tip')
-          .offset([-10,0])
-          .html(function(d){
-            return "<strong>Frequency:</strong> <span style='color:red'>" +d.frequency + "</span>";
-          })
-var svg = d3.select("body").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform","ttranslate(" + margin.left + "," + margin.top + ")");
-svg.call(tip);
-d3.tsv("data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
-
-  svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxis);
-
-  svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis)
-  .append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 6)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end")
-  .text("Frequency");
-
-  svg.selectAll(".bar")
-  .data(data)
-  .enter().append("rect")
-  .attr("class", "bar")
-  .attr("x", function(d) { return x(d.letter); })
-  .attr("width", x.rangeBand())
-  .attr("y", function(d) { return y(d.frequency); })
-  .attr("height", function(d) { return height - y(d.frequency); })
-  .on('mouseover', tip.show)
-  .on('mouseout', tip.hide)
-
-});
-
-function type(d) {
-  d.frequency = +d.frequency;
-  return d;
+function codeAddress(address, callback) {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var position = (results[0].geometry.location);
+      var coordinates = {};
+      coordinates["lat"] = position.A;
+      coordinates["long"] = position.F;
+      callback(coordinates);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
+function plot(){
+  var fd = {
+    "Mount Vernon Fire Department": {
+      "Stations": {
+        "Downtown": {
+          "Location": "50 W. 3rd Street, Mount Vernon, NY",
+          "Call Volume": 420,
+          "Apparatus": {
+            "active": {
+              "Engine 203": {
+                "Response Time": 6,
+                "Unit Call Volume": 325
+              },
+              "Tower Ladder 61": {
+                "Response Time": 7,
+                "Unit Call Volume": 275
+              }
+            },
+            "spare": [
+            "Engine 201",
+            "Engine 207",
+            "Ladder 64"
+            ]
+          }
+        },
+        "West Mount Vernon": {
+          "Location": "9 Oak Street, Mount Vernon, NY",
+          "Call Volume": 325,
+          "Apparatus": {
+            "active": {
+              "Engine 204": {
+                "Response Time": 10,
+                "Unit Call Volume": 234
+              }
+            },
+            "spare": [
+            "Engine 202"
+            ]
+          }
+        },
+        "South Side": {
+          "Location": "435 S. Fulton Avenue, Mount Vernon, NY",
+          "Call Volume": 534,
+          "Apparatus": {
+            "active": {
+              "Engine 205": {
+                "Response Time": 6,
+                "Unit Call Volume": 212
+              },
+              "Ladder 63": {
+                "Response Time": 3,
+                "Unit Call Volume": 120
+              },
+              "Rescue 2": {
+                "Response Time": 4,
+                "Unit Call Volume": 121
+              }
+            },
+            "spare": [
+
+            ]
+          }
+        },
+        "North Side": {
+          "Location": "470 E. Lincoln Avenue, Mount Vernon, NY",
+          "Call Volume": 342,
+          "Apparatus": {
+            "active": {
+              "Engine 206": {
+                "Response Time": 18,
+                "Unit Call Volume": 121
+              },
+              "Ladder 62": {
+                "Response Time": 10,
+                "Unit Call Volume": 123
+              }
+            },
+            "spare": [
+
+            ]
+          }
+        }
+      },
+      "chiefs": {
+        "Car 2290": "Transport Unit",
+        "Car 2291": "Chief of Department",
+        "Car 2292": "Deputy Chief",
+        "Car 2293": "Deputy Chief",
+        "Car 2298": "Support Unit",
+        "Car 2299": "Support Unit"
+      }
+    }
+  };
+  for (var deptName in fd){
+    var dept = fd[deptName];
+    for (var s in dept["Stations"]){
+      var station = dept["Stations"][s];
+      var position = codeAddress(station["Location"], function(c){
+        var marker = new L.marker([c.lat, c.long]).addTo(map);
+      });
+    }
+  }
+}
+var setCenterView = codeAddress("10550", function(c){
+  var lat = c.lat;
+  var long = c.long;
+  var comma = ", ";
+  var coor = lat + comma + long;
+  console.log(coor);
+});
+var map = L.map("map").setView([40.9087956, -73.83308439999996], 14);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiYW5kcmV3YnJvd24iLCJhIjoiNzc2OGRjZTI4YTI0NDViNTAyNDgxNWVhZTM4N2JiNWMifQ.nqxvTAuR8iVPjFK72Qe22A', {
+  attribution: 'Map  data &copy; 2015 Andrew Brown',
+  maxZoom: 18,
+  id: 'andrewbrown.m8aobonh',
+  accessToken: 'pk.eyJ1IjoiYW5kcmV3YnJvd24iLCJhIjoiNzc2OGRjZTI4YTI0NDViNTAyNDgxNWVhZTM4N2JiNWMifQ.nqxvTAuR8iVPjFK72Qe22A'
+}).addTo(map);
+
+plot();
