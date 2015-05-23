ca79ec886fc62e7eaf8b7656dfe6f72b923bcf8f
arbrow06 <arbrow06@syr.edu>
2015-05-23 15:12:22 -0400

    Able to post markers to map but not geographic bubbles.

--- public/js/test.js
+++ public/js/test.js
@@ -8,43 +8,45 @@
         id: 'andrewbrown.m8aobonh',
         accessToken: 'pk.eyJ1IjoiYW5kcmV3YnJvd24iLCJhIjoiNzc2OGRjZTI4YTI0NDViNTAyNDgxNWVhZTM4N2JiNWMifQ.nqxvTAuR8iVPjFK72Qe22A'
         }).addTo(map);
+        /* How to drop a marker at each station
       for (var deptName in fd){
         var dept = fd[deptName];
         for (var s in dept["Stations"]){
           var station = dept["Stations"][s];
           var position = codeAddress(station["Location"], function(c){
-            marker = L.marker([c.lat, c.long]).addTo(map);
-            console.log(c);
+            //marker = L.marker([c.lat, c.long]).addTo(map);
+            //console.log(c);
           });
         }
-      }
+      }*/
+
+
       //Section that places d3 bubbles on map.
+
+      //The overlay plane!
       var svg = d3.select(map.getPanes().overlayPane).append("svg"),
         g = svg.append("g").attr("class", "leaflet-zoom-hide");
-      d3.json("/json/geojson/fd.json", function(c){
-        var transform = d3.geo.transform({point: function(x, y){
+      d3.json("json/geoFd.json", function(collection){
+        function projectPoint(x, y) {
           var point = map.latLngToLayerPoint(new L.LatLng(y, x));
           this.stream.point(point.x, point.y);
-        }});
-        var path = d3.geo.path().projection(transform);
-        d3_features = g.selectAll("path")
-          .data(c.features)
-          .enter().append("path");
-        map.on("viewreset", function(){
-          bounds = path.bounds(c);
-          var topLeft = bounds[0],
-            bottomRight = bounds[1];
-          svg .attr("width", bottomRight[0] - topLeft[0])
-            .attr("height", bottomRight[1] - topLeft[1])
-            .style("left", topLeft[0] + "px")
-            .style("top", topLeft[1] + "px");
-          g .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-          // initialize the path data 
-          d3_features.attr("d", path)
-            .style("fill-opacity", 0.7)
-            .attr('fill','blue');
-        });
-        //console.log(map.getBounds().getEast());
+        }
+        var transform = d3.geo.transform({point: projectPoint}),
+        path = d3.geo.path().projection(transform);
+        var feature = g.selectAll("path")
+        .data(collection.features)
+        .enter().append("path");
+        feature.attr("d", path);
+        var bounds = path.bounds(collection),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+        svg .attr("width", bottomRight[0] - topLeft[0])
+        .attr("height", bottomRight[1] - topLeft[1])
+        .style("left", topLeft[0] + "px")
+        .style("top", topLeft[1] + "px");
+
+        g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
       });
       //End of bubble section
     });
