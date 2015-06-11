09bd668b8dde0e181e31ff744b95f92ce4310b90
arbrow06 <arbrow06@syr.edu>
2015-06-10 21:02:32 -0400

    Updated routes. To develop views

--- app/router.js
+++ app/router.js
@@ -6,6 +6,16 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+	//Default Static Pages
+	this.route('about', {path: '/about'});
+	this.route('faq', {path: '/faq'});
+	this.route('pricing', {path: '/pricing'});
+	this.route('login', {path: '/login'});
+	this.route('signup', {path: '/signup'});
+	//Member Pages
+    this.route('dashboard', {path: '/dashboard'});
+    this.route('settings', {path: '/settings'});
+    //Mapping Routing
 });
 
 export default Router;
